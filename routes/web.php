7d089e25efb4b769051a399e940d9b35637d750e
Katja <you@example.com>
2024-10-23 12:30:27 +0300

    Note controller and get index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('main', [NoteController::class, 'index']);
